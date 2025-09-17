f7ce0dd6d7bee6c4c899ce6e70e69441ca360bc6
Egecher <141852296+Egecher@users.noreply.github.com>
2025-09-17 21:38:41 +0300

    set.dart added

---
+++ set.dart
@@ -0,0 +1,7 @@
+void main(){
+  List<int> sayilarListe = [2, 3, 5, 7, 5]; //List tekrarlı ifadeleri olduğu gibi gösterir.
+  Set<int> sayilarSet = {2, 3, 5, 7, 5}; //Set tekrarlı ifadeleri bir tane olarak gösterir.
+
+  print(sayilarListe.length);
+  print(sayilarSet.length);
+}
