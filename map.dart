390f851a39c2367c4f888b98d6cc34951aaeec96
Egecher <141852296+Egecher@users.noreply.github.com>
2025-09-18 23:37:25 +0300

    map.dart: Added feature to find employee's salary with indexOf

--- map.dart
+++ map.dart
@@ -2,5 +2,6 @@ void main(){
   List<String> calisanlar = ["Ali", "Ayşe", "Fatma", "Ahmet"];
   List<int> maaslar = [9000, 5000, 1200, 7000];
 
-  print(maaslar[2]);
+  int index = calisanlar.indexOf("Fatma");
+  print(maaslar[index]);
 }
