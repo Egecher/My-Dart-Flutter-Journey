db29c04dec3a8300fa74fcdf35751fd228d361d5
Egecher <egemenuysalll@gmail.com>
2025-09-18 23:49:25 +0300

    refactor: replace separate lists with a Map for employee salaries

--- map.dart
+++ map.dart
@@ -1,7 +1,15 @@
 void main(){
-  List<String> calisanlar = ["Ali", "Ayşe", "Fatma", "Ahmet"];
-  List<int> maaslar = [9000, 5000, 1200, 7000];
+  // List<String> calisanlar = ["Ali", "Ayşe", "Fatma", "Ahmet"];
+  // List<int> maaslar = [9000, 5000, 12000, 7000];
 
-  int index = calisanlar.indexOf("Fatma");
-  print(maaslar[index]);
+  // int index = calisanlar.indexOf("Fatma");
+  // print(maaslar[index]);
+
+  Map<String, int> calisanMaaslari = {
+    "Ali": 9000,
+    "Ayşe": 5000,
+    "Fatma": 12000,
+    "Ahmet": 7000
+  };
+  print(calisanMaaslari["Fatma"]);
 }
