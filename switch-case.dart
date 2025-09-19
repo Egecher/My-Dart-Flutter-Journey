ce07116b8d7a803ecf47a18e72be25da67af1ee0
Egecher <egemenuysalll@gmail.com>
2025-09-20 00:21:21 +0300

    Add grade evaluation using switch-case without breaks (fall-through)

---
+++ switch-case.dart
@@ -0,0 +1,16 @@
+void main() {
+  int not = 3;
+
+  switch(not) {
+    case 5:
+    print("Çok iyi");
+    case 4:
+    print("İyi");
+    case 3:
+    print("Orta");
+    case 2:
+    print("Kötü");
+    default:
+    print("Çök kötü");
+  }
+}
