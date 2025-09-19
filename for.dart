c717aca0bea0952cc2539167638a4aa8d755c194
Egecher <141852296+Egecher@users.noreply.github.com>
2025-09-20 01:30:35 +0300

    Add city list and print elements using for loop

---
+++ for.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<String> sehirler = [
+    "İstanbul",
+    "Ankara",
+    "İzmir",
+    "Tekirdağ",
+  ];
+
+  for(int i = 0; sehirler.length > i; i++) {
+    print(sehirler[i]);
+  }
+}
