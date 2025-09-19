8ea06edde88f4a09a252e699a8422173d3a6cca9
Egecher <egemenuysalll@gmail.com>
2025-09-19 23:46:17 +0300

    Refactor age check with single-line ternary assignment

--- ternary.dart
+++ ternary.dart
@@ -1,9 +1,7 @@
 void main() {
   int yas = 25;
 
-  String sonuc;
-
-  sonuc = yas >= 18 ? "Reşitsiniz" : "Henüz reşit değilsiniz";
+  String sonuc = yas >= 18 ? "Reşitsiniz" : "Henüz reşit değilsiniz";
 
   print(sonuc);
 }
