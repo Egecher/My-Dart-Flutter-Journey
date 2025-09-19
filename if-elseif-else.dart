847cb3247c1ba398d5d5ff23de63e4cf72221400
Egecher <141852296+Egecher@users.noreply.github.com>
2025-09-19 23:29:03 +0300

    Add grade evaluation logic

---
+++ if-elseif-else.dart
@@ -0,0 +1,15 @@
+void main(){
+  int not = 75;
+
+  if(not >= 85) {
+    print("Çok iyi");
+  } else if (not > 70) {
+    print("İyi");
+  } else if (not >= 50) {
+    print("Orta");
+  } else if(not >=30) {
+    print("Kötü");
+  } else {
+    print("Çok kötü");
+  }
+}
