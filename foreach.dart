5057fe68cda57865a47b595e8736a69a80d5723f
Egecher <141852296+Egecher@users.noreply.github.com>
2025-09-22 23:39:40 +0300

    docs: add for-in loop example with city list

---
+++ foreach.dart
@@ -0,0 +1,14 @@
+void main() {
+List<String> sehirler = [
+    "İstanbul",
+    "Ankara",
+    "İzmir",
+    "Tekirdağ",
+    "Bursa",
+  ];
+
+
+  for(String sehir in sehirler) { //FOREACH yapısı
+    print(sehir);
+  }
+}
